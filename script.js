18755a167b671ace4e5a8b549af7eba8dce51e5a
YScyber <155051841+YScyber@users.noreply.github.com>
2024-09-09 10:27:00 +0900

    線形グラデーションに関する記述を追加

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const editable = document.querySelector(".editable");
 const textareaHTML = document.querySelector(".playable-html");
 const textareaCSS = document.querySelector(".playable-css");
 const reset = document.getElementById("reset");
+const linear = document.getElementById("linear");
 const radial = document.getElementById("radial");
 const conic = document.getElementById("conic");
 
@@ -13,6 +14,24 @@ function fillCode() {
     section.innerHTML = textareaHTML.value;
     editable.innerHTML = textareaCSS.value;
 }
+
+const linearHtmlSolution = `<p class="linear">線形グラデーションでボーダーを作成</p>`;
+const linearCssSolution = `p {
+    margin: 0 auto;
+    padding: 0.5em;
+    width: 50%;
+    border: 1em solid;
+    text-align: center;
+}
+
+p.linear {
+    border-image: repeating-linear-gradient(
+        -20deg,
+        hsl(239, 100%, 50%),
+        hsl(225, 78%, 82%),
+        hsl(180, 100%, 50%) 30px) 30;
+}
+`;
 
 const radialHtmlSolution = `<p class="radial">放射グラデーションでボーダーを作成</p>`;
 const radialCssSolution = `p {
@@ -54,17 +73,33 @@ p.conic {
 }
 `;
 
+let linearHtmlSolutionEntry = linearHtmlSolution;
 let radialHtmlSolutionEntry = radialHtmlSolution;
 let radialCssSolutionEntry = radialCssSolution;
 
+let linearCssSolutionEntry = linearCssSolution;
 let conicHtmlSolutionEntry = conicHtmlSolution;
 let conicCssSolutionEntry = conicCssSolution;
 
 reset.addEventListener("click", () => {
     textareaHTML.value = htmlCode;
     textareaCSS.value = cssCode;
+    linear.value = "線形グラデーションに変更";
     radial.value = "放射グラデーションに変更";
     conic.value = "扇形グラデーションに変更";
+    fillCode();
+});
+
+linear.addEventListener("click", () => {
+    if (linear.value === "線形グラデーションに変更") {
+        textareaHTML.value = linearHtmlSolutionEntry;
+        textareaCSS.value = linearCssSolutionEntry;
+        linear.value = "元に戻す";
+    } else {
+        textareaHTML.value = htmlCode;
+        textareaCSS.value = cssCode;
+        linear.value = "線形グラデーションに変更";
+    }
     fillCode();
 });
 
