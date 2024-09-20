c65632672146449a41fa844142c1980da445bdbd
YScyber <155051841+YScyber@users.noreply.github.com>
2024-09-20 13:44:57 +0900

    放射グラデーションのコードを削除

--- radial/script.js
+++ radial/script.js
@@ -2,7 +2,6 @@ const section = document.querySelector("section");
 const editable = document.querySelector(".editable");
 const textareaHTML = document.querySelector(".playable-html");
 const textareaCSS = document.querySelector(".playable-css");
-const radial = document.getElementById("radial");
 const reset = document.getElementById("reset");
 
 let htmlCode = textareaHTML.value;
@@ -13,46 +12,9 @@ function fillCode() {
     editable.innerHTML = textareaCSS.value;
 }
 
-const radialHtmlSolution = `<h2>放射グラデーション</h2>
-
-<p class="radial">放射グラデーションでボーダーを作成</p>
-`;
-const radialCssSolution = `p {
-    margin: 0 auto;
-    padding: 0.5em;
-    width: 50%;
-    border: 1em solid;
-    text-align: center;
-}
-
-p.radial {
-    border-image: repeating-radial-gradient(
-        ellipse,
-        hsl(278, 100%, 50%) 0 10px,
-        hsl(0, 7%, 97%) 10px 20px) 10;
-}
-`;
-
-let radialHtmlSolutionEntry = radialHtmlSolution;
-let radialCssSolutionEntry = radialCssSolution;
-
-radial.addEventListener("click", () => {
-    if (radial.value === "放射グラデーションに変更") {
-        textareaHTML.value = radialHtmlSolutionEntry;
-        textareaCSS.value = radialCssSolutionEntry;
-        radial.value = "元に戻す";
-    } else {
-        textareaHTML.value = htmlCode;
-        textareaCSS.value = cssCode;
-        radial.value = "放射グラデーションに変更";
-    }
-    fillCode();
-});
-
 reset.addEventListener("click", () => {
     textareaHTML.value = htmlCode;
     textareaCSS.value = cssCode;
-    radial.value = "放射グラデーションに変更";
     fillCode();
 });
 
