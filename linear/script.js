4cbde1869372f8d226292305a410502561e7553d
YScyber <155051841+YScyber@users.noreply.github.com>
2024-09-20 13:35:00 +0900

    線形グラデーションのコードを削除

--- linear/script.js
+++ linear/script.js
@@ -2,7 +2,6 @@ const section = document.querySelector("section");
 const editable = document.querySelector(".editable");
 const textareaHTML = document.querySelector(".playable-html");
 const textareaCSS = document.querySelector(".playable-css");
-const linear = document.getElementById("linear");
 const reset = document.getElementById("reset");
 
 let htmlCode = textareaHTML.value;
@@ -13,47 +12,9 @@ function fillCode() {
     editable.innerHTML = textareaCSS.value;
 }
 
-const linearHtmlSolution = `<h2>線形グラデーション</h2>
-
-<p class="linear">線形グラデーションでボーダーを作成</p>
-`;
-const linearCssSolution = `p {
-    margin: 0 auto;
-    padding: 0.5em;
-    width: 50%;
-    border: 1em solid;
-    text-align: center;
-}
-
-p.linear {
-    border-image: repeating-linear-gradient(
-        -20deg,
-        hsl(239, 100%, 50%),
-        hsl(225, 78%, 82%),
-        hsl(180, 100%, 50%) 30px) 30;
-}
-`;
-
-let linearHtmlSolutionEntry = linearHtmlSolution;
-let linearCssSolutionEntry = linearCssSolution;
-
-linear.addEventListener("click", () => {
-    if (linear.value === "線形グラデーションに変更") {
-        textareaHTML.value = linearHtmlSolutionEntry;
-        textareaCSS.value = linearCssSolutionEntry;
-        linear.value = "元に戻す";
-    } else {
-        textareaHTML.value = htmlCode;
-        textareaCSS.value = cssCode;
-        linear.value = "線形グラデーションに変更";
-    }
-    fillCode();
-});
-
 reset.addEventListener("click", () => {
     textareaHTML.value = htmlCode;
     textareaCSS.value = cssCode;
-    linear.value = "線形グラデーションに変更";
     fillCode();
 });
 
